f01db744ca671e2a6dd7dfab43d7982bfa2f9fc9
Kiineo <103956412+Kiineo@users.noreply.github.com>
2023-05-01 09:33:11 -0500

    Update RecipeList.js

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -7,19 +7,20 @@ function RecipeList({recipes, deleteRecipe}) {
   // TODO: Create at least one additional component that is used by this component.
   // TODO: Each recipe row must have a delete button - <button name="delete">Delete</button> - that deletes the post when clicked
 
+
+  
   return (
     <div className="recipe-list">
       <table>
-        <thead className="tableHead">
-          
+        <thead>
+          <tr>
             <th>Name</th>
             <th>Cuisine</th>
             <th>Photo</th>
             <th>Ingredients</th>
             <th>Preparation</th>
             <th>Actions</th>
-          
-
+          </tr>
         </thead>
         <tbody>
             {recipes.map((oneRecipe, indx) => <Recipe key={indx} data={oneRecipe} deleteRecipe={deleteRecipe}/>)}
